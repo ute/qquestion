67f91957187a73f1814386004e446a2cbacfdeb9
Ute Hahn <ute@math.au.dk>
2024-02-18 13:40:10 +0100

    quick fix for book projects
    save last question number in a file, and build on top of that. Gives strange results with preview.

--- _extensions/qquestion/qquestion.lua
+++ _extensions/qquestion/qquestion.lua
@@ -1,28 +1,67 @@
 -- nice rename function learned from shafayetShafee :-)
 -- local str = pandoc.utils.stringify
--- local pout = quarto.log.output
+local pout = quarto.log.output
 
--- important quasi global variables
+-- initiate rendering information and global question number
+local utelz = require("./utels")
+local rinfo = {}
 
-local ishtml = quarto.doc.is_format("html")
-local ispdf = quarto.doc.is_format("pdf")
-local qnum = 0
+-- initialize qnum, store and retrieve
+
+local function readqnum(renderinfo)
+  local file = io.open(renderinfo.qnumstore,"r")
+  local qnum = 0
+  if file then 
+    local qnumjson = file:read "*a"
+    file:close()
+    if qnumjson 
+       then qnum = quarto.json.decode(qnumjson)
+    end
+  end  
+  -- pout("got qnum as "..qnum)
+  -- pout(rinfo)
+  return(qnum)
+end;  
+
+local function store_qnum(renderinfo)
+  local qnjson = quarto.json.encode(renderinfo.qnum)
+  local file = io.open(renderinfo.qnumstore,"w")
+  if file ~= nil then 
+    file:write(qnjson) 
+    file:close()
+  end
+end
+ 
+function init_qnum(renderinfo)
+  if renderinfo.ishtmlbook then 
+    if renderinfo.isfirst then 
+      renderinfo.qnum = 0
+    else
+      renderinfo.qnum = readqnum(renderinfo)
+    end
+  else
+    renderinfo.qnum = 0    
+  end  
+end  
+
+-- functions for rendering
 
 local function qstart()
-  qnum = qnum+1
-  if ishtml then
-    return pandoc.RawInline("html",'🤔<sub>[Q'..qnum..']</sub><span class="qquestion" number='..qnum..'>')
-  elseif ispdf then 
-    return pandoc.RawInline("tex",'\\qquestion{'..qnum.."}{")
+  rinfo.qnum = rinfo.qnum+1
+  -- pout("qstart "..rinfo.qnum)
+  if rinfo.ishtml then
+    return pandoc.RawInline("html",'🤔<sub>[Q'..rinfo.qnum..']</sub><span class="qquestion" number='..rinfo.qnum..'>')
+  elseif rinfo.ispdf then 
+    return pandoc.RawInline("tex",'\\qquestion{'..rinfo.qnum.."}{")
   else
     return pandoc.Str("<QStart>")  
   end
 end  
 
 local function qend()
-  if ishtml then
+  if rinfo.ishtml then
     return pandoc.RawInline("html",'</span>')
-  elseif ispdf then 
+  elseif rinfo.ispdf then 
     return pandoc.RawInline("tex",'}')
   else
     return pandoc.Str("<QEnde>")  
@@ -30,10 +69,10 @@ local function qend()
 end  
 
 local function qans()
-  if ishtml then
-    return pandoc.RawInline("html",'</span><span class="qanswer" number='..qnum..'>')
-  elseif ispdf then 
-    return pandoc.RawInline("tex",'}\\qanswer{'..qnum.."}{")
+  if rinfo.ishtml then
+    return pandoc.RawInline("html",'</span><span class="qanswer" number='..rinfo.qnum..'>')
+  elseif rinfo. ispdf then 
+    return pandoc.RawInline("tex",'}\\qanswer{'..rinfo.qnum.."}{")
   else
     return pandoc.Str("<QEnde><QAnswer>")  
   end
@@ -42,7 +81,7 @@ end
 
 -- find {?? bla ??}
 
-Inlines = function(el)
+function Inlines_parse(el)
   for i,ele in pairs(el) do
     if ele.t == "Str" then 
       if ele.text == "{??" then
@@ -59,20 +98,41 @@ Inlines = function(el)
 end
 
 
--- TODO: book, save number between processed files? 
-
-Pandoc = function(doc)
-  if ishtml
+function Pandoc_doit(doc)
+  if rinfo.ishtml
   then 
     quarto.doc.add_html_dependency({
       name = 'qqstyle',
       stylesheets = {'qquestion.css'}
     })
-   elseif ispdf
+   elseif rinfo.ispdf
     then
       quarto.doc.use_latex_package("tcolorbox")
       quarto.doc.include_file('before-body','qquestion.tex')
       quarto.doc.add_format_resource("Emo_think.png")
   end
+ 
+  -- pout("yeah, still have to store qnum "..rinfo.qnum)
+  store_qnum(rinfo)
   return(doc)
 end  
+
+
+return{
+{ -- first get rendering information
+  Meta = function(meta) 
+    rinfo = utelz.Meta_getinfo(meta)
+    rinfo.qnumstore = "_qnumstore.json"
+    init_qnum(rinfo)
+   -- pout("render info ")
+   -- pout("==== book render info ==")
+   -- pout(meta.book.render)
+   -- pout("==== book chapter info ==")
+   -- pout(meta.book.chapters)
+  end  
+},
+{
+  Inlines = Inlines_parse,
+  Pandoc = Pandoc_doit
+}
+}
